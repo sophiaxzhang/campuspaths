604927420d02446d4b18a12034597ca9a1d9fb03
Sophia Zhang <sophiaxzhang@outlook.com>
2025-03-13 11:01:00 -0700

    updated users

--- client/src/users.ts
+++ client/src/users.ts
@@ -1,6 +1,4 @@
 /** List of known users */
 export const USERS: Array<string> = [
-  "Kevin", "James", "Adam", "Alex", "Ali", "Antonio", "Edison", "Jaela", "Katherine", "Jonathan", "Omar", "Noah", "Mayee", "Anmol"
-  // Feel free to change this list to your friends
-  // (or just your favorite of the 331 staff)
+  "Sophia", "Aditi", "Faith", "Maple", "Neha", "Seanna"
 ];
